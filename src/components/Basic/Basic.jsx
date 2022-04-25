c786929717e226631e595cab4f2327971b079309
Lewis Maina Muriuki <43462574+mainamuriuki274@users.noreply.github.com>
2022-04-25 22:35:26 +0300

    (feat): add education and contact details (#4)
    * add education components and info
    * add contact components and info

--- src/components/Basic/Basic.jsx
+++ src/components/Basic/Basic.jsx
@@ -2,7 +2,7 @@ import { About } from '../About';
 import styles from './styles.module.scss';
 
 const Basic = () => (
-  <section className={styles.basic_data}>
+  <section className="detail_section">
     <h1 className={styles.fullname}>Lewis Maina Muriuki</h1>
     <p className={styles.role}>Junior Fullstack Developer</p>
     <About />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import { Basic } from '../Basic';
+import { Education } from '../Education';
 import styles from './styles.module.scss';
 
 const Main = () => (
   <div className={styles.main_details}>
     <Basic />
+    <Education />
   </div>
 );
 
